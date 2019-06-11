bca7509c235074d224db31b959efc9ec0a1fa093
Sam Markham <sam.e.markham@gmail.com>
2019-06-11 18:46:35 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash| 
     if person == "Freddy Mercury"
-      contact_details_hash.each do |category, stuff|
+      contact_details_hash.each do |attribute, stuff|
         if category == :favorite_ice_cream_flavors
           stuff.delete("strawberry")
         end 
